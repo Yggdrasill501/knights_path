8f62e5ead0a6a06377d7d3bed7cab8b3ac9cce44
Filip Žitný <filipzitny@gmail.com>
2023-08-28 00:17:52 +0200

    impl

--- knights_path_impl2/shortest_path.py
+++ knights_path_impl2/shortest_path.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 """Algorythm for shortest path of knight."""
-from random import randint
+from random import randint, choice
+
 
 class ShortestPathKnight:
     """Class for the shortest path of knight"""
 
-    def __int__(self,end_position: tuple,
+    def __int__(self, end_position: tuple,
                 chessboard_size: int = 3,
                 obstacles: bool = False,
-                start_position: tuple = (0,0),
-                random_positions : bool = False) -> None:
-        """Constructor
+                start_position: tuple = (0, 0),
+                random_positions: bool = False) -> None:
+        """Constructor of ShortestPathKnight
 
         :param end_position: end position of knight
         :param chessboard_size: size of chessboard
@@ -25,23 +26,19 @@ class ShortestPathKnight:
         self._obstacles: bool = obstacles
         self._start_position: tuple = start_position
         self._end_position: tuple = end_position
+        self._random: bool = random_positions
 
-    def _generate_chessboard(self) -> list:
+        self._chessboard = []
+
+    def _chessboard_generator(self) -> list:
         """Method Generate chessboard
 
         :return chessboard: generated chessboard
         :rtype: list
         """
-        chessboard = [[_ for i in range(self._chessboard_size)] for _ in range(self._chessboard_size)]
+        self._chessboard = [[0 for _ in range(self._chessboard_size)] for _ in range(self._chessboard_size)]
 
-        return chessboard
-
-    def _positions(self) -> list:
-        """Method for positions of knight
-
-        :return list: list of positions
-        :rtype: list
-        """
+        return self._chessboard
 
     def _obstacles(self) -> list:
         """Method that add random obstacles to the chessboard
@@ -50,9 +47,39 @@ class ShortestPathKnight:
         :rtype: list
         """
 
-        chessboard_with_obstacles = self._generate_chessboard()
+        if self._obstacles:
+            for i in range(self._chessboard_size//3):
+                zero_coords = [(row, col) for row, line in enumerate(self._chessboard) for col, value in enumerate(line) if value == 0]
 
-        return chessboard_with_obstacles
+                if zero_coords:
+                    row, col = choice(zero_coords)
+                    self._chessboard[row][col] = 1
+
+        return self._chessboard
+
+    def _placemnet(self) -> list:
+        """Method that add knight to the chessboard
+
+        :return list: chessboard with knight
+        :rtype: list
+        """
+        if self._random:
+            self._start_position = (randint(0, self._chessboard_size-1), randint(0, self._chessboard_size-1))
+            # TODO: append ranom start position to the chessboard
+            self._end_position = (randint(0, self._chessboard_size-1), randint(0, self._chessboard_size-1))
+            # TODO: append random end position to the chessboard
+
+
+        else:
+            # TODO: append ranom start position to the chessboard
+            # TODO: append random end position to the chessboard
+            pass
+
+        return self._chessboard
+    def BFS(self):
+        # Todo: implement BFS
+        pass
+
 
 if __name__ == '__main__':
     pass
