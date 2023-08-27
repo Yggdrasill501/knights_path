2df070fa84f7a52cc33e1c3b7eaacd3ad1a0c201
Filip Žitný <filipzitny@gmail.com>
2023-08-27 17:34:39 +0200

    user interface

--- knights_path_impl2/user_interface.py
+++ knights_path_impl2/user_interface.py
@@ -14,4 +14,23 @@ class UserInterface:
         :rtype: None
         """
 
+    def talk(self):
+        """
+
+        """
+        print("Hello, this is the program that will show you the shortest path of knight")
+        chessboard_size = int(input("Enter size of chessboard: "))
+
+        game_mode = str(input("if you want to knight to go trough all fields enter 'y', if you want to knight to go to specific field enter 'n': "))
+        if game_mode == 'y':
+            knight_row = 0
+            knight_col = 0
+
+        else:
+            random_position = str(input("Do you want to random position of knight? (y/n): "))
+            if random_position == 'n':
+                knight_row = int(input("Enter row of knight: "))
+                knight_col = int(input("Enter column of knight: "))
+
+        obstacles = str(input("Do you want to add random obstacles? (y/n): "))
 
