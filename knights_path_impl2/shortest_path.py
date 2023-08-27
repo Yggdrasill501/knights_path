b6fb0de5f9b743b591b69e136b46e69c16c9c6d8
Filip Žitný <filipzitny@gmail.com>
2023-08-27 18:38:32 +0200

    init

--- knights_path_impl2/shortest_path.py
+++ knights_path_impl2/shortest_path.py
@@ -1,31 +1,58 @@
 # -*- coding: utf-8 -*-
 """Algorythm for shortest path of knight."""
+from random import randint
 
-
-class ShortestPath:
+class ShortestPathKnight:
     """Class for the shortest path of knight"""
 
-    def __int__(self, chessboard_size: int = 3, obstacles: bool = False, ) -> None:
+    def __int__(self,end_position: tuple,
+                chessboard_size: int = 3,
+                obstacles: bool = False,
+                start_position: tuple = (0,0),
+                random_positions : bool = False) -> None:
         """Constructor
 
+        :param end_position: end position of knight
         :param chessboard_size: size of chessboard
         :param obstacles: if True, obstacles will be generated
         :param obstacles: if false, obstacles will not be generated
+        :param start_position: start position of knight
+        :param random_positions: if True, start position will be generated randomly
         :rtype: None
         """
 
-        self.chessboard_size: int = chessboard_size
-        self.obstacles: bool = obstacles
+        self._chessboard_size: int = chessboard_size
+        self._obstacles: bool = obstacles
+        self._start_position: tuple = start_position
+        self._end_position: tuple = end_position
 
-    def generate_chessboard(self) -> list:
+    def _generate_chessboard(self) -> list:
         """Method Generate chessboard
 
         :return chessboard: generated chessboard
         :rtype: list
         """
-        chessboard = [[_ for i in range(self.chessboard_size)] for _ in range(self.chessboard_size)]
+        chessboard = [[_ for i in range(self._chessboard_size)] for _ in range(self._chessboard_size)]
 
         return chessboard
+
+    def _positions(self) -> list:
+        """Method for positions of knight
+
+        :return list: list of positions
+        :rtype: list
+        """
+
+    def _obstacles(self) -> list:
+        """Method that add random obstacles to the chessboard
+
+        :return list: chessboard with obstacles
+        :rtype: list
+        """
+
+        chessboard_with_obstacles = self._generate_chessboard()
+
+        return chessboard_with_obstacles
 
 if __name__ == '__main__':
     pass
