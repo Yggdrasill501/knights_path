e38592e31ee0a2b40553e85cf7525f351109557b
Filip Žitný <filipzitny@gmail.com>
2023-08-28 00:22:59 +0200

    trubleshooting

--- knights_path_impl2/user_interface.py
+++ knights_path_impl2/user_interface.py
@@ -2,7 +2,7 @@
 import logging
 import argparse
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
 
 
 class UserInterface:
@@ -14,23 +14,38 @@ class UserInterface:
         :rtype: None
         """
 
-    def talk(self):
-        """
-
-        """
+    def user_interface(self):
+        """Method that interact with user"""
         print("Hello, this is the program that will show you the shortest path of knight")
-        chessboard_size = int(input("Enter size of chessboard: "))
+        try:
+            chessboard_size = int(input("Enter size of chessboard: "))
+        except ValueError:
+            logging.error("Invalid input type")
+            return -1
 
-        game_mode = str(input("if you want to knight to go trough all fields enter 'y', if you want to knight to go to specific field enter 'n': "))
+        try:
+            game_mode = str(input("if you want to knight to go trough all fields enter 'y', if you want to knight to go to specific field enter 'n': "))
+        except ValueError:
+            logging.error("Invalid input type")
+            return -1
+
         if game_mode == 'y':
             knight_row = 0
             knight_col = 0
 
         else:
-            random_position = str(input("Do you want to random position of knight? (y/n): "))
+            try:
+                random_position = str(input("Do you want to random position of knight? (y/n): "))
+            except ValueError:
+                logging.error("Invalid input type")
+                return -1
+
             if random_position == 'n':
                 knight_row = int(input("Enter row of knight: "))
                 knight_col = int(input("Enter column of knight: "))
 
-        obstacles = str(input("Do you want to add random obstacles? (y/n): "))
-
+        try:
+            obstacles = str(input("Do you want to add random obstacles? (y/n): "))
+        except ValueError:
+            logging.error("Invalid input type")
+            return -1
