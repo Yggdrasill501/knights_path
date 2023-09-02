14676dddbffe4875ce49e9d58ee8f990bb0e4093
Filip Žitný <filipzitny@gmail.com>
2023-09-02 18:53:35 +0200

    Rework

--- user_interface.py
+++ user_interface.py
@@ -2,7 +2,6 @@
 """File with user interface."""
 import logging
 from knights_path.shortest_path import ShortestPathKnight
-import time
 
 logging.basicConfig(level=logging.ERROR)
 LOGGER = logging.getLogger(__name__)
@@ -14,5 +13,4 @@ def user_interface(chessboard_file):
     print("Please enter file with chessboard, start position as S and end position as E, or edit existing one.")
     print("Minimal chessboard size is 3x3")
     shortest_path = ShortestPathKnight(chessboard_file)
-    time.sleep(1)
     print(f"Minimal number of steps to reach end position is {shortest_path.breath_first_search()}")
