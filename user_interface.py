daadd1355dddb99f4aba32a8555a2ca65cd8372e
Filip Žitný <filipzitny@gmail.com>
2023-09-18 22:50:35 +0200

    Rework

--- user_interface.py
+++ user_interface.py
@@ -12,5 +12,11 @@ def user_interface(chessboard_file):
     print("Welcome to the shortest path of knight.")
     print("Please enter file with chessboard, start position as S and end position as E, or edit existing one.")
     print("Minimal chessboard size is 3x3")
+
+    x = input("Enter file name: ")
+
     shortest_path = ShortestPathKnight(chessboard_file)
+
+
+
     print(f"Minimal number of steps to reach end position is {shortest_path.breath_first_search()}")
