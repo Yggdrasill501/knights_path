8a828e5db6f3bb6300bc0bc792ebd4613af5a691
Filip Žitný <filipzitny@gmail.com>
2023-09-19 16:52:48 +0200

    update for merging

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 # -*- coding: utf-8 -*-
 """Main file, runs whole code."""
-from user_interface import user_interface
-
-if __name__ == '__main__':
-    user_interface(chessboard_file="chessboard.txt")
