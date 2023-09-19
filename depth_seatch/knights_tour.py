2986db5614a329d0d7de37af720b7674e5d3aba0
Filip Žitný <filipzitny@gmail.com>
2023-09-19 16:26:50 +0200

    Warnsdorff's rule

--- depth_seatch/knights_tour.py
+++ depth_seatch/knights_tour.py
@@ -21,11 +21,11 @@ class KnightsTour:
         self.moves_row = [2, 1, -1, -2, -2, -1, 1, 2]
         self.moves_colum = [1, 2, 2, 1, -1, -2, -2, -1]
 
-    def _is_valid_move(self, row, colum):
+    def _is_valid_move(self, row: int, colum: int):
         """Private method that says if the move is valid.
 
-        :param row: row of the chessboard
-        :param colum: colum of the chessboard
+        :param row: int, row of the chessboard
+        :param colum: int, colum of the chessboard
         :returns True: if the move is valid
         :returns False: if the move is not valid
         :rtype bool:
@@ -37,4 +37,17 @@ class KnightsTour:
             MODULE_LOGGER.info("No valid move")
             return False
 
+    def warnsdorffs_count(self, row, colum):
+        """Method with implementation of Warnsdorff's rule.
 
+        :param row: int, row of the chessboard
+        :param colum: int, colum of the chessboard
+        :return count: int, count of valid moves
+        :rtype int:
+        """
+        count = 0
+        for i in range(self.chessboard_size):
+            new_row, new_colum = row + self.moves_row[i], colum + self.moves_colum[i]
+            if self._is_valid_move(new_row, new_colum):
+                count += 1
+        return count
